c600dd9a663c0e51d681a1bcf7148e59abc29631
Marc Dos Santos <mdossantosdev@gmail.com>
2020-04-21 14:51:17 +0200

    Create check.js to commands folder and update coindex-check.js

--- bin/coindex-check.js
+++ bin/coindex-check.js
@@ -1,8 +1,11 @@
 const program = require('commander');
+const check = require('../commands/check');
 
 program
   .command('price')
   .description('Check price of crypto')
-  .action(() => console.log('Price'));
+  .option('--coin <type>', 'Add specific crypto ticker', 'BTC, ETH, LINK')
+  .option('--cur <currency', 'Change the currency', 'USD')
+  .action((cmd) => check.price(cmd));
 
 program.parse(process.argv);
